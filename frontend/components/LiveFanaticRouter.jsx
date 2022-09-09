c07fbdd21a530fe1f9fb83f7d8ccb185ae2a8ca9
M0ller <simon.mollerberg@gmail.com>
2022-09-09 20:26:37 +0200

    removed old files

--- frontend/components/LiveFanaticRouter.jsx
+++ frontend/components/LiveFanaticRouter.jsx
@@ -1,27 +1,14 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import AudioTest from "./AudioTest";
-import VideoTest from "./VideoTest";
-import MainPage from "./MainPage";
-import VideoAudioTest from "./VideoAudioTest";
+
 
 
 function LiveFanaticRouter(){
     return <Router>
         <header>
-            {/* <Link to="/" id="exampleID"><h1>Main header</h1></Link> */}
-             <h1 id="exampleID">Main header</h1>
         </header>
         <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/audio-test" element={<AudioTest />} />
-            <Route path="/video-test" element={<VideoTest />} />
-            <Route path="/video-audio-test" element={<VideoAudioTest />} />
         </Routes>
         <footer>
-{/*             <nav>
-                <h1>CopyRight example</h1>
-                <Link to="/">Main page</Link>
-            </nav> */}
         </footer>
     </Router>
 }
