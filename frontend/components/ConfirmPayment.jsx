089c7dbb22228981af77e27f29cbf16021f8d809
LukasLarss <Lukasslarsson@outlook.com>
2022-09-12 16:42:49 +0200

    Update ConfirmPayment.jsx

--- frontend/components/ConfirmPayment.jsx
+++ frontend/components/ConfirmPayment.jsx
@@ -7,26 +7,22 @@ function ConfirmPayment(){
     <div className="confirm-payment">
         <div className="confirm-payment-left-content">
             <div className="total-tickets" >
-                <h2 className="space-between">Total Tickets: <span>x 4</span></h2>
+                <h2 className="space-between">Total Tickets: <span>x 1</span></h2>
                 
             </div>
             <div className="your-tickets">
                 <h2>Your Tickets:</h2>
                 <div className="ticket-list-container">
                     <ol className="ticket-container" >
-                        <li>sabaton x 1 </li>
-                        <li>primo victoria x 2</li>
-                        <li>sailermoon x 1 </li>
+                        <li>Slam Poetry Bonga Free Jazz Jam for Dummies x 1 </li>
                     </ol>
                     <ul className="ticket-amount-container" >
-                        <li>100 SEK</li>
-                        <li>200 SEK</li>
-                        <li>100 SEK</li>
+                        <li>299.00 SEK</li>
                     </ul>
                 </div>
             </div>
             <div className="total">
-                <h2 className="space-between">Total: <span>400 SEK</span></h2>
+                <h2 className="space-between">Total: <span>299.00 SEK</span></h2>
             </div>
             <Link to="/buy-ticket">
                 <button>Cancel</button>
@@ -39,7 +35,7 @@ function ConfirmPayment(){
             </div>
 
             <div className="sprite-container">
-                sprite container goes here
+                spripe container goes here
             </div>
         </div>
     </div>    
