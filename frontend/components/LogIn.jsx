d19f51e09e74ef36caa0ae6ab80e76f6e3dcded3
M0ller <simon.mollerberg@gmail.com>
2022-09-14 13:15:52 +0200

    login with button hardcoded

--- frontend/components/LogIn.jsx
+++ frontend/components/LogIn.jsx
@@ -1,22 +1,65 @@
 import { Link } from "react-router-dom";
+import  { useState} from "react"
+import { useEffect } from "react";
+import LoginPost from "./LoginPost"
+
+function Example() {
+    // Declare a new state variable, which we'll call "count"  
+    const [count, setCount] = useState(0);
+    return (
+      <div>
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>
+          Click me
+        </button>
+      </div>
+    );
+  }
+
+  function test(){
+    useEffect(()=>{
+        (async()=>{
+            let res = await fetch('/data/login', {
+                method: 'POST',
+                headers:{
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify({
+                    email: userEmail,
+                    password: userPass
+                })
+            })
+            console.log(res)
+            console.log("I got called")
+        })
+    },[])
+  }
 
 function LogIn(){
+
+ /*    const handleSubmit=(e)=>{
+
+    }
+ */
     return <>
     <div className="body">
         <form className="login">
             <h2>Log in</h2>
+            
             <div className = "email">
-                <input type="text" className="input-text" placeholder="Email"/>
+                <input className="input-text" placeholder="Email" type="text" />
             </div>
     
             <div className="password">
-                <input type="password" className="input-text" placeholder="Password"/>
+                <input className="input-text" placeholder="Password" type="text" />
             </div>
             
             <div className="buttons">
-                <Link to="/log-in">
-                <button className ="login-page-btn" type="submit">Log in</button>
-                </Link>
+                <button className ="login-page-btn" type="submit" 
+                onClick={LoginPost}
+                /* onClick={()=> useEffect} */
+                >Log in</button>
+
                 <Link to="/sign-up">
                 <button className ="login-page-signup-btn" type="submit">Sign up</button>
                 </Link>
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,11 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import LiveFanaticRouter from '../components/LiveFanaticRouter'
 
 function App() {
 
   return <>
-    <LiveFanaticRouter/>
-    
-  {/* <h2>This is a placeholder page, change for your react build directory</h2>
-    <p>
-        <a href="/examples">Examples of audio and video streaming here</a>
-    </p> */}
-    
+    <LiveFanaticRouter/>  
   </>
 }
 
