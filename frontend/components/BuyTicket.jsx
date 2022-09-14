bf1126f8ac0d22d0afcafb4b945d5724374d572a
M0ller <simon.mollerberg@gmail.com>
2022-09-14 09:44:35 +0200

    adjusted buy-ticket page

--- frontend/components/BuyTicket.jsx
+++ frontend/components/BuyTicket.jsx
@@ -2,59 +2,78 @@ import { Link } from "react-router-dom";
 
 function BuyTicket() {
   return <>
-      <div className="body">
-        <div className="containerBuy">
-          <div className="leftBuy">
-            <h1 className="buy-ticket-h1">Buy Your Ticket</h1>
-            <Link to="/artist">
-              <img
-                className="buy-ticket-img"
-                src="https://i.ytimg.com/vi/88ki680YlB4/maxresdefault.jpg"
-                alt=""
-              />
-            </Link>
+    <div className="body">
 
+      <h1 className="buy-ticket-h1">Buy Your Ticket</h1>
       <div className="containerBuy">
         <div className="leftBuy">
-          <h1 className="buy-ticket-h1">Buy Your Ticket</h1>
+          <div>
           <Link to="/artist">
-          <img className="buy-ticket-img"
-            src="../images/franaddressescrowd500.jpg"
-            alt=""
-          />
+            <img
+              className="buy-ticket-img"
+              src="https://i.ytimg.com/vi/88ki680YlB4/maxresdefault.jpg"
+              alt=""
+            />
           </Link>
+          </div>
 
-          <h2 className="buy-ticket-h2">Info:</h2>
+          <div>
+            <div className="buy-ticket-info-container">
+          <h2 className="buy-ticket-h2-info">Info:</h2>
           <p className="buy-ticket-p">Slam Poetry Bonga Free Jazz Jam for Dummies</p>
+            </div>
+            <div className="buy-ticket-info-container artist-info-container">
+          <h2 className="buy-ticket-h2-artist">Artists:</h2>
+            <div>
+            <p className="buy-ticket-p">Static plants</p>
+            <p className="buy-ticket-p">Purge!</p>
+            <p className="buy-ticket-p">Cacti pillow</p>
+            </div>
+            </div>
+            <div className="buy-ticket-info-container">
+            <h2 className="buy-ticket-h2">Date:</h2>
+            <p className="buy-ticket-p">Thursday 8 september 2022 7:30pm</p>
+            </div>
 
-          <h2 className="buy-ticket-h2">Artists:</h2>
-          <p className="buy-ticket-p">Static plants <br /> Purge! <br />Cacti pillow</p>
-
-          <h2 className="buy-ticket-h2">Date:</h2>
-          <p className="buy-ticket-p">Thursday 8 september 2022 7:30pm</p>
-
-          <h2 className="buy-ticket-h2">Location:</h2>
-          <p className="buy-ticket-p">Andy's garage</p>
-        </div>
-
-            <h2 className="buy-ticket-h2">Ticket:</h2>
-            <h2 className="buy-ticket-h2">x 1</h2>
-
-            <h2 className="buy-ticket-h2">Total:</h2>
-            <h2 className="buy-ticket-h2">299.00 SEK</h2>
-
-            <Link to="/confirm-payment">
-              <button
-                className="buy-ticket-btn"
-                onclick="document.location=/confirm-payment"
-              >
-                Checkout
-              </button>
-            </Link>
+            <div className="buy-ticket-info-container">
+            <h2 className="buy-ticket-h2">Location:</h2>
+            <p className="buy-ticket-p">Andy's garage</p>
+            </div>
           </div>
         </div>
+
+        <div className="rightBuy">
+
+          <div className="rightBuy-subContainer">
+          <h2 className="buy-ticket-h2">Ticket:</h2>
+          <h2 className="buy-ticket-h2">x 1</h2>
+          </div>
+
+          <div className="rightBuy-subContainer">
+          <h2 className="buy-ticket-h2">Tickets:</h2>
+          <h2 className="buy-ticket-h2">x 1</h2>
+          </div>
+
+          <div className="rightBuy-subContainer">
+          <h2 className="buy-ticket-h2">Total:</h2>
+          <h2 className="buy-ticket-h2">299.00 SEK</h2>
+          </div>
+
+          <div className="rightBuy-btn">
+          <Link to="/confirm-payment">
+            <button
+              className="buy-ticket-btn"
+              onclick="document.location=/confirm-payment"
+            >
+              Checkout
+            </button>
+          </Link>
+          </div>
+
+        </div>
+
       </div>
-      </div>
+    </div>
   </>
 }
 
