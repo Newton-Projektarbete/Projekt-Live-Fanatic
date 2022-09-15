9ae849afd0a662941750fc98e471652ac36471c7
M0ller <simon.mollerberg@gmail.com>
2022-09-15 15:25:30 +0200

    added concerts sorted by recently added

--- frontend/components/Main.jsx
+++ frontend/components/Main.jsx
@@ -2,6 +2,13 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from 'react';
 
 function Main() {
+
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = today.getFullYear();
+    today = dd + '-' + mm + '-' + yyyy;
+    
     useEffect(() => {
         async function load() {
             let concerts = await fetch('data/concert/')
@@ -53,11 +60,6 @@ function Main() {
         }
     ])
 
-    let today = new Date();
-    let dd = String(today.getDate()).padStart(2, '0');
-    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    let yyyy = today.getFullYear();
-    today = dd + '-' + mm + '-' + yyyy;
 
     const liveConcertsToday = () => {
         let concertArr = []
