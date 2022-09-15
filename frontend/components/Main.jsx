d5d679925d7d6a16658d1c9064f52699f70d7b2c
Jonatan <jonatan@Jonatans-MBP.lan>
2022-09-15 11:56:40 +0200

    concert variables main

--- frontend/components/Main.jsx
+++ frontend/components/Main.jsx
@@ -1,15 +1,56 @@
+import { Link } from "react-router-dom";
 import {useState, useEffect} from 'react';
-import LoadConcert from './loadConcert';
-
-
 
 function Main(){
+    useEffect( ()=>{
+        async function load(){
+        let concerts = await fetch('data/concert/1')
+        concerts = await concerts.json()
+        console.log(concerts)
+        variableUpdateMethod(concerts)
+        }
+        load()
+    },[])
+
+    const [variableName, variableUpdateMethod] = useState([])
+
+return <>
+<div className="body">
+
+        <div className="main-content-page">
+            <div className="main-content-header">
+                <h1 className="main-h1" >Live concerts</h1>
+                <Link to="/main-view-all" className="view_all">View all</Link>
+            </div>
+            <div className="row">
+            <div className="main-content-box">
+
+                    <div className="main-img-box"> <img className="main-img" src="../design-html/images/girl-group-itzy-attend-a-red-carpet-event-at-the-mnet-asian-news-photo-1639422911.jpg" alt=""/>
+                        <div to="" onclick="Toggle()" className="material-symbols-outlined main-like-btn">
+                            <span className="like-btn-1 material-symbols-outlined">favorite</span>
+                        </div>
+                    </div>
+
+                    <div className="child-div">
+                      <em>{variableName.map(concert => 
+        <Link to="" key={concert.concert_id}>
+            {concert.concert_name}
+        </Link>)}</em>
+        <em>{variableName.map(concert => 
+        <Link to="" key={concert.concert_id}>
+            {concert.artist_name}
+        </Link>)}</em>
+                    </div>
+                </div>
+                
+
+          </div>
+        </div>
+
+
     
-
-return<LoadConcert></LoadConcert>
-
- 
+</div>
+</>
 }
-
 
 export default Main
