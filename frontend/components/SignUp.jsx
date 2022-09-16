b46c3f263c67bfdea09d139602bc49bb65df876c
Daniel Perrson Lerdin <daniel.persson-lerdin@edu.newton.com>
2022-09-16 16:07:11 +0200

    figuring signup out. Not done

--- frontend/components/SignUp.jsx
+++ frontend/components/SignUp.jsx
@@ -1,30 +1,58 @@
-import { Link } from "react-router-dom";
+import { Link, Navigate, useResolvedPath } from "react-router-dom";
+import { useState } from "react";
 
 function SignUp(){
+
+    const [user, setUser] = useState({
+        user_id: 0,
+        email: "",
+        password: "",
+        roles: ""
+    })
+
+    const handleOnSubmit = event => {
+        event.preventDefault();
+        Navigate('/success', {
+            state: {
+                email: user.email,
+                password: user.password
+            }
+        })
+    } 
+
+    const handleChange = event =>{
+        setUser({...user, [event.target.name]: event.target.value})
+      };
+
+    const signupUser = () =>{
+         fetch('/data/users', {
+            method: 'POST',
+            body: JSON.stringify({
+                email: user.email,
+                password: user.password,
+            })
+    });
+    }
+
+       
+
+
     return <>
     <div className="body">
-    <div clasName="signup-body">
-        <form className="signup">
+    <div className="signup-body">
+        <form className="signup" onSubmit={handleOnSubmit}>
             <h2>Sign up</h2>
             <div className = "email-signup">
-                <input type="text" className="input-text" placeholder="Email"/>
-            </div>
-    
-            <div className = "username">
-                <input type="text" className="input-text" placeholder="Username"/>
+                <input type="email" className="input-text" onChange={handleChange} placeholder="Email"/>
             </div>
     
             <div className="password">
-                <input type="password" className="input-text" placeholder="Password"/>
-            </div>
-    
-            <div className="password">
-                <input type="password" className="input-text" placeholder="Repeat password"/>
+                <input type="text" className="input-text"  onChange={handleChange} placeholder="Password"/>
             </div>
             
             <div className="signup-btn-box">
-                <Link to="#" className ="parent-signup-page-btn">
-                <input className ="signup-page-btn" type="submit" value="Sign up"/>
+                <Link to="/" className ="parent-signup-page-btn">
+                <input className ="signup-page-btn" onClick={signupUser} type="submit" value="Sign up"/>
                 </Link>
                 <Link to="/log-in" className= "link">Already have an account? Login
                 </Link>
