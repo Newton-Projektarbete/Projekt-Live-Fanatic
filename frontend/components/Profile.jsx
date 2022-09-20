6132e803566ff7e075d397fecf1fc83c0e9e4c2c
Liha86 <sanjin.elfynd@gmail.com>
2022-09-20 14:26:20 +0200

    expired tickets done

--- frontend/components/Profile.jsx
+++ frontend/components/Profile.jsx
@@ -15,12 +15,56 @@ function Profile(){
     today = dd + '-' + mm + '-' + yyyy;
 
 
-    const myTickets = () => {
+    const activeTickets = () => {
         let tickets = []
 
         for (let i = 0; i < sortedConcerts.length; i++) {
             for (let x = 0; x < allTickets.length; x++) {
                if (sortedConcerts[i].concert_id == allTickets[x].concert_id && sortedConcerts[i].performance_date >= today) {
+                
+                tickets[i] = 
+                <table>
+                    <tr className="table-header">
+                        <th></th>
+                        <th>Concert:</th>
+                        <th>Date:</th>
+                        <th>Location:</th>
+                    </tr>
+    
+                    <tr>
+                        <td className="profile-td"><img className="img-profile" src={sortedConcerts[i].concert_image_url} /></td>
+                        <td className="profile-td"><Link to="">{sortedConcerts[i].concert_name}</Link></td>
+                        <td className="profile-td"><p>{sortedConcerts[i].performance_date}</p></td>
+                        <td className="profile-td">{sortedConcerts[i].location}</td>
+                        <td className="profile-td">
+                        
+                            <a href="#">
+                                <span className="material-symbols-outlined global-icons">print</span>
+                            </a>
+                            <a href="#">
+                                <span className="material-symbols-outlined global-icons">qr_code</span>
+                            </a>
+                        </td>
+    
+                    </tr>
+    
+                </table> 
+               }
+                
+            }
+            
+        }
+        return tickets 
+
+    }
+
+
+    const expiredTickets = () => {
+        let tickets = []
+
+        for (let i = 0; i < sortedConcerts.length; i++) {
+            for (let x = 0; x < allTickets.length; x++) {
+               if (sortedConcerts[i].concert_id == allTickets[x].concert_id && sortedConcerts[i].performance_date < today) {
                 
                 tickets[i] = 
                 <table>
@@ -71,41 +115,13 @@ function Profile(){
             <div className="main-table-content">
 
             <div className="left-table">
-            <h2>My tickets</h2>
-            {myTickets()}
+            <h2>Active tickets</h2>
+            {activeTickets()}
             </div> 
                 
                 <div className="centrum-table">
                     <h2>Expired tickets </h2>
-                    <table>
-                        <tr className="table-header">
-                            <th></th>
-                            <th>Concert:</th>
-                            <th>Date:</th>
-                            <th>Location:</th>
-                        </tr>
-
-                        <tr>
-                            <td className="profile-td"><img className="img-profile" src="../examples/justin-timberlake.jpg" /></td>
-                            <td className="profile-td"><a href="">Justin Timberlake</a></td>
-                            <td className="profile-td">2022.12.12</td>
-                            <td className="profile-td">Stockholm, Globen</td>
-                        </tr>
-
-                        <tr>
-                            <td className="profile-td"><img className="img-profile" src="../examples/justin-timberlake.jpg" /></td>
-                            <td className="profile-td"><a href="">Justin Timberlake</a></td>
-                            <td className="profile-td">2022.12.12</td>
-                            <td className="profile-td">Stockholm, Globen</td>
-                        </tr>
-
-                        <tr>
-                            <td className="profile-td"><img className="img-profile" src="../examples/justin-timberlake.jpg" /></td>
-                            <td className="profile-td"><a href="">Justin Timberlake</a></td>
-                            <td className="profile-td">2022.12.12</td>
-                            <td className="profile-td">Stockholm, Globen</td>
-                        </tr>
-                    </table>
+                    {expiredTickets()}         
                 </div>
 
                 <div></div>
